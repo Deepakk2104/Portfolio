217a783d73b7bea8ac24579346303ad3aa970b86
Deepak Kumar <deepakk12435@gmail.com>
2025-11-08 23:44:16 +0530

    update

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -24,15 +24,23 @@ const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
   return (
-    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
-      <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
-        <Link
-          href={"/"}
-          className="text-2xl md:text-5xl text-white font-semibold"
-        >
-          LOGO
-        </Link>
-        <div className="mobile-menu block md:hidden">
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] border-b border-[#33353F]">
+      <div className="container mx-auto flex items-center justify-between px-4 py-3 lg:py-4">
+        {/* Logo */}
+
+        {/* Centered Links (Desktop) */}
+        <div className="hidden md:flex justify-center flex-1">
+          <ul className="flex space-x-10">
+            {navLinks.map((link, index) => (
+              <li key={index}>
+                <NavLink href={link.path} title={link.title} />
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {/* Mobile Menu Button */}
+        <div className="block md:hidden">
           {!navbarOpen ? (
             <button
               onClick={() => setNavbarOpen(true)}
@@ -49,16 +57,9 @@ const Navbar = () => {
             </button>
           )}
         </div>
-        <div className="menu hidden md:block md:w-auto" id="navbar">
-          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
-            {navLinks.map((link, index) => (
-              <li key={index}>
-                <NavLink href={link.path} title={link.title} />
-              </li>
-            ))}
-          </ul>
-        </div>
       </div>
+
+      {/* Mobile Menu Overlay */}
       {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
     </nav>
   );

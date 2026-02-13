d079f5863ab3f0e1165e48495122a7cc36995330
Deepak Kumar <deepakk12435@gmail.com>
2026-02-13 13:45:30 +0530

    Fix typo in HeroSection component description

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -39,7 +39,7 @@ const HeroSection = () => {
           </h1>
 
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Transforming ideas into smooth, interactive web experiences
+            Transforming ideas into smooth, interactive web experience
           </p>
 
           <div>

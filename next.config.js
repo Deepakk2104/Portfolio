5e713b65ddcd752a763d846764b07ae207d066a0
Deepak Kumar <deepakk12435@gmail.com>
2026-08-20 17:50:54 +0530

    Fix: replace deprecated images.domains with images.remotePatterns

--- next.config.js
+++ next.config.js
@@ -2,7 +2,16 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ["localhost", "127.0.0.1"],
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+      },
+    ],
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1024, 1200],
     imageSizes: [16, 32, 48, 64, 96, 128, 256],
